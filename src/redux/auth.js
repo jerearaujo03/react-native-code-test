473fe811ea93b4a0a3287f2f28a2480f06b0441f
Jeremias Araujo <jeremias@decemerlabs.com>
2021-08-27 01:18:36 -0300

    add auth reducer and action creators

--- src/redux/auth.js
+++ src/redux/auth.js
@@ -1,12 +1,59 @@
+import * as api from '../api/index';
+
 const initialState = {
-  currentUser: {},
+  pending: false,
+  user: null,
 };
 
+const SIGN_IN_PENDING = 'AUTH/SIGN_IN_PENDING';
+const SIGN_IN_FULFILLED = 'AUTH/SIGN_IN_FULFILLED';
+const SIGN_IN_REJECTED = 'AUTH/SIGN_IN_REJECTED';
+const SIGN_OUT = 'AUTH/SIGN_OUT';
+
 export default (state = initialState, action) => {
-  // eslint-disable-next-line no-unused-vars
   const {type, payload} = action;
   switch (type) {
+    case SIGN_IN_PENDING:
+      return {
+        ...state,
+        pending: true,
+        error: '',
+      };
+    case SIGN_IN_FULFILLED:
+      return {
+        ...state,
+        pending: false,
+        error: '',
+        user: payload,
+      };
+    case SIGN_IN_REJECTED:
+      return {
+        ...state,
+        pending: false,
+        error: payload,
+      };
+    case SIGN_OUT:
+      return {
+        ...state,
+        pending: false,
+        error: '',
+        user: null,
+      };
     default:
       return state;
   }
 };
+
+export const signIn = (username, password) => async dispatch => {
+  dispatch({type: SIGN_IN_PENDING});
+  try {
+    const result = await api.signIn(username, password);
+    dispatch({type: SIGN_IN_FULFILLED, payload: result.user});
+  } catch (e) {
+    dispatch({type: SIGN_IN_REJECTED, payload: e.error});
+  }
+};
+
+export const signOut = () => ({
+  type: SIGN_OUT,
+});
